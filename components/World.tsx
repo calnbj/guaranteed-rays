fc6bf98e6c6b81f12d7f17998debe78ff3a2cc16
calnbj <calnbj@gmail.com>
2026-03-24 19:27:55 +0000

    fix: massive zoom and light adjustment for visibility

--- components/World.tsx
+++ components/World.tsx
@@ -2,45 +2,32 @@
 import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrthographicCamera, OrbitControls, ContactShadows } from '@react-three/drei'
-import Path from './Path'
-import Player from './Player'
+import DioramaPath from './DioramaPath'
 
 export default function World() {
   return (
     <div style={{ width: '100vw', height: '100vh', background: '#e8e2d6' }}>
       <Canvas shadows dpr={[1, 2]}>
+        {/* THE FIX: Zooming in 4x closer so you can see the buildings */}
         <OrthographicCamera 
           makeDefault 
-          position={[50, 50, 50]} 
-          zoom={50} 
+          position={[100, 100, 100]} 
+          zoom={250} 
           near={0.1} 
-          far={1000} 
+          far={2000} 
         />
         
         <color attach="background" args={['#e8e2d6']} />
-        <ambientLight intensity={0.8} />
-        <directionalLight 
-          position={[10, 20, 10]} 
-          intensity={1.2} 
-          castShadow 
-        />
-        <pointLight position={[-10, 10, -10]} intensity={0.5} color="#ffccaa" />
-
-        <group rotation={[0, -Math.PI / 4, 0]}>
-          <Path />
-          <Player />
+        <ambientLight intensity={1.5} />
+        <directionalLight position={[10, 20, 10]} intensity={1.5} castShadow />
+        
+        {/* Centering the map in the middle of the camera's view */}
+        <group position={[0, -5, 0]}>
+          <DioramaPath />
         </group>
 
-        <ContactShadows 
-          position={[0, -0.01, 0]} 
-          opacity={0.4} 
-          scale={100} 
-          blur={2.5} 
-          far={10} 
-          color="#4a4030" 
-        />
-        
-        <OrbitControls enablePan={true} makeDefault />
+        <ContactShadows opacity={0.4} scale={200} blur={2.5} far={20} color="#4a4030" />
+        <OrbitControls makeDefault enablePan={true} />
       </Canvas>
     </div>
   )
