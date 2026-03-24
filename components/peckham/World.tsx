8044ae8d6aaec59ac15a2a58f510897154210154
calnbj <calnbj@gmail.com>
2026-03-24 19:02:49 +0000

    style: monument valley isometric camera and studio lighting

--- components/peckham/World.tsx
+++ components/peckham/World.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, PerspectiveCamera, ContactShadows } from '@react-three/drei'
+import { OrbitControls, OrthographicCamera, ContactShadows } from '@react-three/drei'
 import { Landmark } from './Landmark'
 import { RoadTile, ParkTile } from './Environment'
 import { mapNodes } from '../../lib/mapData'
@@ -8,20 +8,48 @@ import { mapNodes } from '../../lib/mapData'
 export default function World() {
   return (
     <div className="w-full h-screen bg-[#e8e2d6]">
-      <Canvas shadows>
-        <PerspectiveCamera makeDefault position={[-15, 15, -15]} fov={35} />
-        <OrbitControls makeDefault maxPolarAngle={Math.PI / 2.1} />
+      <Canvas shadows dpr={[1, 2]}>
+        {/* THE MONUMENT VALLEY CAMERA: Orthographic makes it look like a toy */}
+        <OrthographicCamera 
+          makeDefault 
+          position={[40, 40, 40]} 
+          zoom={80} 
+          near={0.1} 
+          far={200} 
+        />
+        
+        <OrbitControls 
+          enablePan={true} 
+          enableRotate={true} 
+          enableZoom={true}
+          minZoom={40}
+          maxZoom={120}
+        />
+
         <color attach="background" args={['#e8e2d6']} />
-        <fog attach="fog" args={['#e8e2d6', 10, 50]} />
-        <ambientLight intensity={0.8} />
-        <directionalLight position={[10, 20, 10]} intensity={1} castShadow />
-        {mapNodes.map((node: any) => {
-          if (node.type === 'pub') return <Landmark key={node.id} node={node} />
-          if (node.type === 'road') return <RoadTile key={node.id} node={node} />
-          if (node.type === 'park') return <ParkTile key={node.id} node={node} />
-          return null
-        })}
-        <ContactShadows position={[0, 0, 0]} opacity={0.4} scale={20} blur={2} />
+        
+        {/* Soft, even lighting like a studio photo */}
+        <ambientLight intensity={0.9} />
+        <directionalLight position={[10, 20, 10]} intensity={0.5} />
+        <pointLight position={[-10, -10, -10]} intensity={0.2} color="#ffccaa" />
+
+        <group rotation={[0, -Math.PI / 4, 0]}>
+          {mapNodes.map((node: any) => (
+            <group key={node.id} position={[node.x * 2.5, 0, node.z * 2.5]}>
+              {node.type === 'pub' ? <Landmark node={node} /> : <RoadTile node={node} />}
+            </group>
+          ))}
+        </group>
+
+        {/* Soft Contact Shadows are ESSENTIAL for this look */}
+        <ContactShadows 
+          position={[0, -0.01, 0]} 
+          opacity={0.3} 
+          scale={100} 
+          blur={3} 
+          far={10} 
+          color="#4a4030"
+        />
       </Canvas>
     </div>
   )
